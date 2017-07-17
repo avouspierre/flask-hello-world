cbe958c75f44585371af9d959c14b3a0944efb1c
avouspierre <pn.lagarde@gmail.com>
2017-07-17 22:42:47 +0200

    version 0.5 tutoral

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,20 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_openid import OpenID
+from config import basedir
+
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+lm = LoginManager(app)
+lm.login_view = 'login'
+oid = OpenID(app,os.path.join(basedir,'tmp'))
+
 
 from app import views, models
--- app/models.py
+++ app/models.py
@@ -1,13 +1,33 @@
-from app import db
+from app import db,lm
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key=True)
+    social_id = db.Column(db.String(64),nullable=False, unique=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)
+        except NameError:
+            return str(self.id)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -17,3 +37,5 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post %r>' % (self.body) 
+
+
